20cc79f897ab9830d14c86027efaec79253930dd
ejohnfel <ejohnfel@outlook.com>
2020-07-31 11:19:44 -0400

    Changed addons to .bash_profile for interactive logins, and code to clean from .bashrc

--- remove.sh
+++ remove.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-TARGET=~/.bash_profile
+TARGET=~/.bashrc
 TMP=/tmp/bashrc_prefix.${RANDOM}
 MARKER="\\[AUTOMATED-INSERT-MARKER\\]"
 UPDATE=bashrc
-OLD=~/.bashrc
 
 # make
 
-if [ -f ${UPDATE} ]; then
+if [ -f ${TARGET} ]; then
 	echo -e "[= Detecting Marker..."
 
 	if grep -q "${MARKER}" ${TARGET}; then
-		OUTPUT=`grep -n "${MARKER}" ${TARGET}`
+		OUTPUT=$(grep -n "${MARKER}" ${TARGET})
 
 		if [ ! "${OUTPUT}" = "" ]; then
 			echo "[= Detected Marker"
@@ -23,22 +22,15 @@ if [ -f ${UPDATE} ]; then
 			echo -e "[=== Clearing out old stuff..."
 			sed -n "1,${INDEX}p" ${TARGET} > ${TMP}
 			echo -e "[==== Updating ${TARGET}"
-			cat ${TMP} ${UPDATE} > ${TARGET}
+			cat ${TMP} > ${TARGET}
 			echo -e "[=== Done"
 			rm ${TMP}
 		else
 			echo "[== Detected Nothing, Can't Complete"
 		fi
 	else
-		echo -e "[== No Marker Found, Adding Addendum..."
-		cat ${UPDATE} >> ${TARGET}
-		echo -e "[= Done"
+		echo -e "[== No Marker Found, we good..."
 	fi
 else
-	echo -e "Can't find update file: ${UPDATE}"
-fi
-
-if grep -q "${MARKER}" ${OLD}; then
-	printf "Detected marker in ${OLD}, removing from there..."
-	./remove.sh
+	echo -e "Can't find target file: ${TARGET}"
 fi
