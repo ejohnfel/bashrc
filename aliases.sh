18c1c8a4a08ce90dd483723dfe0f02bafe13aff4
ejohnfel <ejohnfel@outlook.com>
2020-08-19 22:35:00 -0400

    Minor change to leases alias, setting to 'cat' versus 'less' so one can process the output

--- aliases.sh
+++ aliases.sh
@@ -4,4 +4,4 @@ alias rootme="sudo -s"
 alias cls=clear
 alias ils="docker image ls | tail -n +2 | while read name tag image created just now size other; do printf \"\${name} \${tag} \${image}\n\"; done"
 alias j="journalctl -xe"
-alias leases="[ -f /var/lib/dhcp/dhcpd.leases ] && less /var/lib/dhcp/dhcpd.leases || printf 'No lease file on this host\n'"
+alias leases="[ -f /var/lib/dhcp/dhcpd.leases ] && cat /var/lib/dhcp/dhcpd.leases || printf 'No lease file on this host\n'"
