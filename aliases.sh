9dda93057ed306a8887ad5154f4caba2dc8adf5c
ejohnfel <ejohnfel@outlook.com>
2020-08-19 22:28:34 -0400

    Changed ver number, added some aliases

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,6 @@
 
 alias rootme="sudo -s"
 alias cls=clear
+alias ils="docker image ls | tail -n +2 | while read name tag image created just now size other; do printf \"\${name} \${tag} \${image}\n\"; done"
+alias j="journalctl -xe"
+alias leases="[ -f /var/lib/dhcp/dhcpd/lease ] && less /var/lib/dhcp/dhcpd.leases || printf 'No lease file on this host\n'"
