688391f6f13259d74ece9de814c56674ffe654bd
ejohnfel <ejohnfel@hotmail.com>
2026-01-15 22:40:46 -0500

    Altered CDPATH

--- bashrc.sh
+++ bashrc.sh
@@ -5,7 +5,7 @@
 # Title: Main bashrc file
 # Purpose: Is the ~/.bashrc file
 
-BASHRCVERSION="202601131903"
+BASHRCVERSION="202601152232"
 
 # Source Aliases
 [ -f ~/.bash_aliases ] && source ~/.bash_aliases
